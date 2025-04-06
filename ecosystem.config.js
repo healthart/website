de1b36662b0685fdb31a531b25a5c40dd573f855
Max <max@coinsummer.io>
2025-04-06 15:23:55 +0800

    fix: pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,14 @@
-export default {
+module.exports = {
   apps: [{
     name: 'health-art',
-    script: 'pnpm',
+    script: '/usr/bin/pnpm',
     args: 'dev',
-    interpreter: 'none',
-    env: {
-      NODE_ENV: 'production'
-    },
     cwd: '/home/ubuntu/workspace/health-art',
     instances: 1,
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
-    env_production: {
+    env: {
       NODE_ENV: 'production'
     }
   }]
